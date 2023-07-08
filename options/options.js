0c21dd4e4214e104353009bb8f82e584204a37a3
Crowdbotics <team@crowdbotics.com>
2023-07-08 12:09:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-boat-41999",
+  url: "https://billowing-boat-41999.botics.co",
+  api: "https://billowing-boat-41999.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
